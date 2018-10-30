55ecc465ebb231bcb5586c61343ae03d8688a6dd
Georgios Fragkos <g.fragkos@yandex.com>
2018-10-30 21:53:11 +0200

    Testing new method of generating content
    Added some more content and changed the way thiw content is generated.

---
+++ about.js
@@ -0,0 +1,13 @@
+var myvar = '<!DOCTYPE html>'+
+				'<html>'+
+				'	<head>'+
+				'		<title>Demo Index Page</title>'+
+				'	</head>'+
+				'	<body>'+
+				'		<p><h2>About</h2></p>'+
+				'		<p>Who we are and what we do.</p>'+
+				'		<p>Site under construction...</p>'+
+				'	</body>'+
+				'</html>';
+				
+document.write(myvar);
